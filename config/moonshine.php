d993a64fe2e1c16a909997392174eb62e0a53442
Nikita V <swebs.sh@gmail.com>
2024-06-19 12:04:27 +0500

    Установил moonshine

---
+++ config/moonshine.php
@@ -0,0 +1,111 @@
+<?php
+
+use MoonShine\Exceptions\MoonShineNotFoundException;
+use MoonShine\Forms\LoginForm;
+use MoonShine\Http\Middleware\Authenticate;
+use MoonShine\Http\Middleware\SecurityHeadersMiddleware;
+use MoonShine\Models\MoonshineUser;
+use MoonShine\MoonShineLayout;
+use MoonShine\Pages\ProfilePage;
+
+return [
+    'dir' => 'app/MoonShine',
+    'namespace' => 'App\MoonShine',
+
+    'title' => env('MOONSHINE_TITLE', 'MoonShine'),
+    'logo' => env('MOONSHINE_LOGO', 'images/mam_logo_hor_left_white.svg'),
+    'logo_small' => env('MOONSHINE_LOGO_SMALL', 'images/logo_small_white.svg'),
+
+    'route' => [
+        'domain' => env('MOONSHINE_URL', ''),
+        'prefix' => env('MOONSHINE_ROUTE_PREFIX', 'crm'),
+        'single_page_prefix' => 'page',
+        'index' => 'moonshine.index',
+        'middlewares' => [
+            SecurityHeadersMiddleware::class,
+        ],
+        'notFoundHandler' => MoonShineNotFoundException::class,
+    ],
+
+    'use_migrations' => true,
+    'use_notifications' => true,
+    'use_theme_switcher' => true,
+
+    'layout' => MoonShineLayout::class,
+
+    'disk' => 'public',
+
+    'disk_options' => [],
+
+    'cache' => 'file',
+
+    'assets' => [
+        'js' => [
+            'script_attributes' => [
+                'defer',
+            ]
+        ],
+        'css' => [
+            'link_attributes' => [
+                'rel' => 'stylesheet',
+            ]
+        ]
+    ],
+
+    'forms' => [
+        'login' => LoginForm::class
+    ],
+
+    'pages' => [
+        'dashboard' => App\MoonShine\Pages\Dashboard::class,
+        'profile' => ProfilePage::class
+    ],
+
+    'model_resources' => [
+        'default_with_import' => true,
+        'default_with_export' => true,
+    ],
+
+    'auth' => [
+        'enable' => true,
+        'middleware' => Authenticate::class,
+        'fields' => [
+            'username' => 'email',
+            'password' => 'password',
+            'name' => 'name',
+            'avatar' => 'avatar',
+        ],
+        'guard' => 'moonshine',
+        'guards' => [
+            'moonshine' => [
+                'driver' => 'session',
+                'provider' => 'moonshine',
+            ],
+        ],
+        'providers' => [
+            'moonshine' => [
+                'driver' => 'eloquent',
+                'model' => MoonshineUser::class,
+            ],
+        ],
+        'pipelines' => [],
+    ],
+    'locales' => [
+        'en',
+        'ru',
+    ],
+
+    'global_search' => [
+        // User::class
+    ],
+
+    'tinymce' => [
+        'file_manager' => false, // or 'laravel-filemanager' prefix for lfm
+        'token' => env('MOONSHINE_TINYMCE_TOKEN', ''),
+        'version' => env('MOONSHINE_TINYMCE_VERSION', '6'),
+    ],
+
+    'socialite' => [
+        // 'driver' => 'path_to_image_for_button'
+    ],
+];
